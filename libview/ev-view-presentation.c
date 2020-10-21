lebucur/evince

commit b2b9012c5e326f08a2347441c6a88b28b60a9f02

presentation: fix too large slides on scaled display
which happens due to GTK regression in the reported
sizes obtained from gdk_monitor_get_geometry()
function which changed behaviour starting with
GTK 3.24.9. See gtk#2599 .

Fixes #1365

libview/ev-view-presentation.c
@@ -1287,8 +1287,15 @@ ev_view_presentation_update_monitor_geometry (EvViewPresentation *pview)
 	monitor = gdk_display_get_monitor_at_window (display, window);
 	gdk_monitor_get_geometry (monitor, &geometry);
 
+#if GTK_CHECK_VERSION(3, 24, 9)
+	/* See Evince issue #1365 and GTK regression gtk#2599 */
+	int scale_factor = gdk_monitor_get_scale_factor (monitor);
+	pview->monitor_width = geometry.width / scale_factor;
+	pview->monitor_height = geometry.height / scale_factor;
+#else
 	pview->monitor_width = geometry.width;
 	pview->monitor_height = geometry.height;
+#endif
 }
 
 static gboolean

commit 6e9a70d5b52fccc01458a71708bdee307453bd7a

presentation mode: fix X11 regression
caused by my commit b2b9012c5e because
it added a wayland fix without ensuring
it'll be used in Wayland only.

Fixes #1504

libview/ev-view-presentation.c
@@ -30,6 +30,9 @@
 #include "ev-transition-animation.h"
 #include "ev-view-cursor.h"
 #include "ev-page-cache.h"
+#ifdef GDK_WINDOWING_WAYLAND
+#include <gdk/gdkwayland.h>
+#endif
 
 enum {
 	PROP_0,
@@ -1287,14 +1290,16 @@ ev_view_presentation_update_monitor_geometry (EvViewPresentation *pview)
 	monitor = gdk_display_get_monitor_at_window (display, window);
 	gdk_monitor_get_geometry (monitor, &geometry);
 
-#if GTK_CHECK_VERSION(3, 24, 9)
-	/* See Evince issue #1365 and GTK regression gtk#2599 */
-	int scale_factor = gdk_monitor_get_scale_factor (monitor);
-	pview->monitor_width = geometry.width / scale_factor;
-	pview->monitor_height = geometry.height / scale_factor;
-#else
 	pview->monitor_width = geometry.width;
 	pview->monitor_height = geometry.height;
+
+#if GTK_CHECK_VERSION(3, 24, 9) && defined (GDK_WINDOWING_WAYLAND)
+	if (GDK_IS_WAYLAND_DISPLAY (display)) {
+		/* See Evince issue #1365 and GTK regression gtk#2599 */
+		int scale_factor = gdk_monitor_get_scale_factor (monitor);
+		pview->monitor_width = geometry.width / scale_factor;
+		pview->monitor_height = geometry.height / scale_factor;
+	}
 #endif
 }
 